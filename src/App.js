502a625ee4579b897b0c61e3e3b1a10590e33cb6
João Vitor Corrêa <joaov1997@hotmail.com>
2023-05-09 15:13:43 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
+import { generos } from './generos';
 
 function App() {
+
+  const apiKey = 'a11b2c0cd5fb0efb5878106808068b4f'
+  const [movie,setMovie] = useState()
+
+  let page = 1
+  
+  const handleGenreClick = async (genreId, total_pages) => {
+    if (total_pages > 500) total_pages = 500 
+
+    page = Math.floor(Math.random() * total_pages)
+    if (page === 0) page = 1
+
+    const requestUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}&page=${page}&language=pt-BRvote_count.gte=1000sort_by=vote_average.desc&vote_average.gte=${7}&vote_average.lte=10&include_adult=false&certification_country=US&certification=R`;
+
+    await fetch(requestUrl)
+      .then(response => response.json())
+      .then(data => {
+        const randomIndex = Math.floor(Math.random() * 20);
+        const movie = data.results[randomIndex];
+        console.log(data.total_pages)
+        console.log(movie)
+        setMovie(movie);
+      })
+      .catch(error => alert('Não existe filmes com esses filtros'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="genre-container">
+        <div>
+        {generos.map(genero =>{
+          return <button key={genero.id} onClick={()=>{handleGenreClick(genero.id,genero.total_pages)}}>{genero.name}</button>
+          })}
+        </div>
+      </div>
+      <div className="App">
+      {movie ?
+        <>
+        <div className='filme_data'>
+          <p> {movie?.original_title}</p>
+        </div>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`} alt="Poster do filme não disponível!" />
+        <div className='filme_data'>
+          <p> {movie?.overview ? movie?.overview : 'Sinopse não disponível' }</p>
+        </div>
+        </>
+        : 
+        <h1>Selecione o genero para gerar uma recomendação de filme</h1>
+      }
+      </div>
+    </>
   );
 }
 
---
+++ src/generos.js
@@ -0,0 +1,96 @@
+export const generos = [
+      {
+        "id": 28,
+        "name": "Ação",
+        "total_pages": 16
+      },
+      {
+        "id": 12,
+        "name": "Aventura",
+        "total_pages": 5
+      },
+      {
+        "id": 16,
+        "name": "Animação",
+        "total_pages": 4
+      },
+      {
+        "id": 35,
+        "name": "Comédia",
+        "total_pages": 22
+      },
+      {
+        "id": 80,
+        "name": "Crime",
+        "total_pages": 19
+      },
+      {
+        "id": 99,
+        "name": "Documentário",
+        "total_pages": 9
+      },
+      {
+        "id": 18,
+        "name": "Drama",
+        "total_pages": 52
+      },
+      // {
+      //   "id": 10751,
+      //   "name": "Família",
+      //   "total_pages": 1
+      // },
+      {
+        "id": 14,
+        "name": "Fantasia",
+        "total_pages": 4
+      },
+      {
+        "id": 36,
+        "name": "História",
+        "total_pages": 7
+      },
+      {
+        "id": 27,
+        "name": "Terror",
+        "total_pages": 10
+      },
+      {
+        "id": 10402,
+        "name": "Música",
+        "total_pages": 6
+      },
+      {
+        "id": 9648,
+        "name": "Mistério",
+        "total_pages": 7
+      },
+      {
+        "id": 10749,
+        "name": "Romance",
+        "total_pages": 12
+      },
+      {
+        "id": 878,
+        "name": "Ficção científica",
+        "total_pages": 5
+      },
+      // {
+      //   "id": 10770,
+      //   "name": "Cinema TV"
+      // },
+      {
+        "id": 53,
+        "name": "Thriller",
+        "total_pages": 23
+      },
+      {
+        "id": 10752,
+        "name": "Guerra",
+        "total_pages": 5
+      },
+      {
+        "id": 37,
+        "name": "Faroeste",
+        "total_pages": 2
+      }  
+]
